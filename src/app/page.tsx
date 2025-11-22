0657ebf1d5b2f29ce77dc11e2dde4bf718356f6b
Fernando <66154723+fernandosmither@users.noreply.github.com>
2025-11-22 00:33:27 -0300

    fix pnpm run build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import Image from "next/image";
 import { useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
+import { type Doc } from "../../convex/_generated/dataModel";
 
 export default function Home() {
   const tasks = useQuery(api.tasks.get);
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {tasks?.map(({ _id, text }) => (
-        <div key={_id}>{text}</div>
+      {tasks?.map((task: Doc<"tasks">) => (
+        <div key={task._id}>{task.text}</div>
       ))}
     </main>
   );
